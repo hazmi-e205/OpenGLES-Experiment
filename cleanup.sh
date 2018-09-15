967714a223034c6a0961af21cfa600ba69c84ca4
Hazmi F47 <hazmiarifin@gmail.com>
2018-09-15 16:06:55 +0700

    - modify cleanup method
    add revert all changes

--- cleanup.sh
+++ cleanup.sh
@@ -5,3 +5,6 @@ git clean -fdx
 # -f: force git clean to delete the files
 # -d: delete untracked directories in addition to untracked files
 # -x: delete files that are ignored by git
+
+git checkout .
+# revert changes made to your working copy
