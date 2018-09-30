1fdca917d305ecd0c0b577e2c0cffdabc912344c
Hazmi F47 <hazmiarifin@gmail.com>
2018-09-30 18:35:43 +0700

    * Fix Android build
    + add stdafx.h
    * fix load file flow

--- OGLES/Sources/Speak.h
+++ OGLES/Sources/Speak.h
@@ -6,11 +6,9 @@
 #define OGLES_SPEAK_H
 #include "PlatformDefine.h"
 
-
-
 #if defined (AndroidStudio)
 #include <android/log.h>
-const char* LOG_TAG = "HazmiOGL";
+#define LOG_TAG "HazmiOGL"
 
 #define  Speak(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define  Problem(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
---
+++ OGLES/app/src/main/cpp/stdafx.h
@@ -0,0 +1,10 @@
+//
+// Created by hazmi on 30/09/18.
+//
+
+#ifndef OGLES_STDAFX_H
+#define OGLES_STDAFX_H
+
+//nothing to do
+
+#endif //OGLES_STDAFX_H
