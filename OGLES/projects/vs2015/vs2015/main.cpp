4a715874c81c7b85afa5c583370238c2c303bfef
Hazmi F47 <hazmiarifin@gmail.com>
2018-09-30 09:06:36 +0700

    ! GLFW and GLEW Applied
    + lib files
    - modify win32 solution

--- OGLES/projects/vs2015/vs2015/main.cpp
+++ OGLES/projects/vs2015/vs2015/main.cpp
@@ -2,10 +2,51 @@
 //
 
 #include "stdafx.h"
+#include "GL\glew.h"
+#include "GLFW/glfw3.h"
 
-
-int main()
+int main(void)
 {
-    return 0;
-}
+	GLFWwindow* window;
 
+	/* Initialize the library */
+	if (!glfwInit())
+		return -1;
+
+	/* Create a windowed mode window and its OpenGL context */
+	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	if (!window)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Make the window's context current */
+	glfwMakeContextCurrent(window);
+
+	/* GLEW initialize */
+	if (glewInit() != GLEW_OK)
+	{
+		glfwTerminate();
+		return -1;
+	}
+
+	/* Native initialize */
+	//setupGraphics(640, 480);
+
+	/* Loop until the user closes the window */
+	while (!glfwWindowShouldClose(window))
+	{
+		/* Render here */
+		//renderFrame();
+
+		/* Swap front and back buffers */
+		glfwSwapBuffers(window);
+
+		/* Poll for and process events */
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
+}
